54b45ab0cd43e49e2b7dcd9d76ab16344d64c164
José Tobias <jose.tobias@outlook.com>
2020-01-13 04:59:30 -0300

    Implement the admin area controller Index method.

--- CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CMSShoppingCart.Infrastructure;
+using CMSShoppingCart.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CMSShoppingCart.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PagesController : Controller
     {
-        public string Index()
+        private readonly CMSShoppingCartContext context;
+
+        public PagesController(CMSShoppingCartContext context)
         {
-            return "admin test";
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+            List<Page> pagesList = await pages.ToListAsync();
+            return View(pagesList);
         }
     }
 }
