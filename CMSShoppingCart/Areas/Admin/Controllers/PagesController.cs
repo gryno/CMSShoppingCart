50f28cc373deced81a549e12a72121ecabfced56
José Tobias <jose.tobias@outlook.com>
2020-01-15 01:20:26 -0300

    Add the details page method handler on controller and view page.

--- CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -19,11 +19,25 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
             this.context = context;
         }
 
+        // GET /admin/pages
         public async Task<IActionResult> Index()
         {
             IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
             List<Page> pagesList = await pages.ToListAsync();
+           
             return View(pagesList);
+        }
+        
+        // GET /admin/pages/details/id?
+        public async Task<IActionResult> Details(int id)
+        {
+            Page page = await context.Pages.FirstOrDefaultAsync(x => x.Id == id);
+            if(page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
         }
     }
 }
