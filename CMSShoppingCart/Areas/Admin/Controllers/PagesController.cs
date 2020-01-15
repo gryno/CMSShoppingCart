49e380739bce455e106144b725a7b1236fad3b6b
José Tobias <jose.tobias@outlook.com>
2020-01-15 04:08:34 -0300

    Create the edit controller method and page. Added validation the not let duplications exist.

--- CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -59,7 +59,7 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
                 var slug = await context.Pages.FirstOrDefaultAsync(x => x.Slug == page.Slug);
                 if (slug != null)
                 {
-                    ModelState.AddModelError("", "The title already exists.");
+                    ModelState.AddModelError("", "The page already exists.");
 
                     return View(page);
                 }
@@ -70,6 +70,47 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
                 TempData["Success"] = "The page has been created!";
 
                 return RedirectToAction("Index");
+            }
+
+            return View(page);
+        }
+
+        // GET /admin/pages/edit/id?
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            Page page = await context.Pages.FindAsync(id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+
+        // POST /admin/pages/edit/id?
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                page.Slug = page.Id == 1 ? "home" : page.Title.ToLower().Replace(" ", "-");
+
+                var slug = await context.Pages.Where(x => x.Id != page.Id).FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The page already exists.");
+
+                    return View(page);
+                }
+
+                context.Update(page);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The page has been edited!";
+
+                return RedirectToAction("Edit", new { id = page.Id });
             }
 
             return View(page);
