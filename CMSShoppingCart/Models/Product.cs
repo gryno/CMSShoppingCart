fbcfe5a06954cbd847acfc9cdf8fede8b9bc3910
José Tobias <jose.tobias@outlook.com>
2020-01-16 07:01:20 -0300

    Add range validation for category id dbo field.

--- CMSShoppingCart/Models/Product.cs
+++ CMSShoppingCart/Models/Product.cs
@@ -20,6 +20,7 @@ namespace CMSShoppingCart.Models
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         [Display(Name = "Category")]
+        [Range(1, int.MaxValue, ErrorMessage = "You must choose a category")]
         public int CategoryId { get; set; }
         [Display(Name = "Product Image")]
         public string Image { get; set; }
