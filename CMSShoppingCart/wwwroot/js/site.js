37a01ff4dbcfedb4597248d311aad4201a761af1
José Tobias <jose.tobias@outlook.com>
2020-01-16 06:59:18 -0300

    Add the product preview for uploaded images.

--- CMSShoppingCart/wwwroot/js/site.js
+++ CMSShoppingCart/wwwroot/js/site.js
@@ -18,3 +18,15 @@ $(function () {
         });
     }
 });
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+
+        reader.onload = function (e) {
+            $("img#ImagePreview").attr("src", e.target.result).width(200).height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
