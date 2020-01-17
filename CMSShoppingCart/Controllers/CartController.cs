77df341ca122ab38a0434a67cf14515d947bf5eb
José Tobias <jose.tobias@outlook.com>
2020-01-17 03:16:28 -0300

    Create session extension to serialize and deserialize session json objects.

---
+++ CMSShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMSShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMSShoppingCart.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly CMSShoppingCartContext context;
+
+        public CartController(CMSShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
