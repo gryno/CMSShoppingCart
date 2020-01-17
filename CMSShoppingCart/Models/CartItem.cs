a00f66c105218c8e169e814da20cc9583f6170c8
José Tobias <jose.tobias@outlook.com>
2020-01-17 09:24:36 -0300

    Refactor the get method for the total property of the CartItem class.

--- CMSShoppingCart/Models/CartItem.cs
+++ CMSShoppingCart/Models/CartItem.cs
@@ -11,7 +11,7 @@ namespace CMSShoppingCart.Models
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
-        public decimal Total { get; set; }
+        public decimal Total { get { return Quantity * Price; } }
         public string Image { get; set; }
 
         public CartItem(Product product)
